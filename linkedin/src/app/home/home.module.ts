84aac1b564d0b872bbe329fd1bd3aae71481411b
JobaAdewumi <jobaadewumis@gmail.com>
2021-11-30 10:02:28 +0100

    first commit

---
+++ linkedin/src/app/home/home.module.ts
@@ -0,0 +1,20 @@
+import { HeaderComponent } from './component/header/header.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [HomePage, HeaderComponent]
+})
+export class HomePageModule {}
