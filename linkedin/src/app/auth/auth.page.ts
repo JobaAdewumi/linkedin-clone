5c4857b1e637db0bd982feb14b04321262b9d7c0
JobaAdewumi <jobaadewumis@gmail.com>
2021-12-01 16:14:34 +0100

    Mobile Responsiveness

---
+++ linkedin/src/app/auth/auth.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.page.html',
+  styleUrls: ['./auth.page.scss'],
+})
+export class AuthPage implements OnInit {
+  @ViewChild('form') form: NgForm;
+
+  submissionType: 'login' | 'join' = 'login'
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+
+  }
+
+
+}
